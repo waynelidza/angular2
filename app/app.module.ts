15d7dee8680c3df7fc42a0d3f8f215c379c931c9
Geoffrey Lydall <geoffrey.lydall@dariel.co.za>
2016-10-20 13:37:11 +0200

    Add ignore for generated artifacts in app

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,13 +7,14 @@ import { AdminComponent } from './components/admin/admin.component';
 import { StdProvisioningComponent } from './components/stdprovisioning/stdprovisioning.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppComponent }  from './app.component';
-import {FeedbackComponent} from'./components/feedback/feedback.component';
+import { FeedbackComponent } from'./components/feedback/feedback.component';
+import { StreamingListComponent } from './components/streaming-list/streaming-list.component';
 
 import { AppRoutingModule }     from './app.routes';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule, HttpModule, AppRoutingModule],
-  declarations: [ AppComponent, AdminComponent, StdProvisioningComponent, HomeComponent,FeedbackComponent],
+  declarations: [ AppComponent, AdminComponent, StdProvisioningComponent, HomeComponent, FeedbackComponent, StreamingListComponent],
   bootstrap: [ AppComponent ]
 })
 
