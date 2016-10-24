c6ea9f70831dca6e66de331c1122c5274b4bb370
Geoffrey Lydall <geoffrey.lydall@dariel.co.za>
2016-10-24 14:38:25 +0200

    OR-53 Fixed app.comoponent unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,37 +1,31 @@
 /* tslint:disable:no-unused-variable */
 import { AppComponent } from './app.component';
 
-import { TestBed }      from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Router, RouterOutlet } from "@angular/router";
+import {RouterLinkStubDirective, RouterOutletStubComponent } from '../testing/router-stubs'
 
-import { By }           from '@angular/platform-browser';
+describe('AppComponent with TCB', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({declarations: [AppComponent, RouterLinkStubDirective, RouterOutletStubComponent]});
+    });
 
-////////  SPECS  /////////////
+    it('should instantiate component', () => {
+      let fixture = TestBed.createComponent(AppComponent);
+      expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
+    });
 
-/// Delete this
-describe('Smoke test', () => {
-  it('should run a passing test', () => {
-    expect(true).toEqual(true, 'should pass');
-  });
-});
+    it('should have expected links', () => {
+      let fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
 
-describe('AppComponent with TCB', function () {
-  beforeEach(() => {
-    TestBed.configureTestingModule({declarations: [AppComponent]});
-  });
+      let linkElements = fixture.debugElement.queryAll(By.directive(RouterLinkStubDirective));
+      let links = linkElements.map(de => de.injector.get(RouterLinkStubDirective) as RouterLinkStubDirective);
 
-  it('should instantiate component', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
-  });
-
-  it('should have expected <h1> text', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-
-    let h1 = fixture.debugElement.query(el => el.name === 'h1').nativeElement;  // it works
-
-        h1 = fixture.debugElement.query(By.css('h1')).nativeElement;            // preferred
-
-    expect(h1.innerText).toMatch(/angular app/i, '<h1> should say something about "Angular App"');
-  });
+      expect(links.length).toBe(4);
+      expect(links[1].linkParams).toBe('/admin');
+      expect(links[2].linkParams).toBe('/std');
+      expect(links[3].linkParams).toBe('/feedback');
+    });
 });
