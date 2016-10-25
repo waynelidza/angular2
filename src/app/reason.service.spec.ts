9f2e3dfe352b43de9c507053b94ae9d6782bde08
Geoffrey Lydall <geoffrey.lydall@dariel.co.za>
2016-10-25 14:51:49 +0200

    OR-52 added helpers for HTTP mocking

--- src/app/reason.service.spec.ts
+++ src/app/reason.service.spec.ts
@@ -1,41 +1,20 @@
 /* tslint:disable:no-unused-variable */
 import { TestBed, inject, async, getTestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { Http, HttpModule, BaseRequestOptions, Response, ResponseOptions, XHRBackend } from '@angular/http';
-import { MockBackend, MockConnection } from '@angular/http/testing'
-import {Observable} from 'rxjs/Rx';
+import { MockBackend } from '@angular/http/testing'
+import { Observable } from 'rxjs/Rx';
 
 import { ReasonService } from './reason.service';
-
-// describe('smoke', () => {
-//   it ('should fail', () =>{
-//     expect(true).toBeFalsy();
-//   })
-// })
-
+import { HttpMockResponse } from '../testing/http-responses';
 
 describe('Reason Service', () => {
   let mockBackend: MockBackend;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        ReasonService,
-        MockBackend,
-        BaseRequestOptions,
-        {
-          provide: Http,
-          deps: [MockBackend, BaseRequestOptions],
-          useFactory:
-            (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
-              return new Http(backend, defaultOptions);
-            }
-       }
-      ],
-      imports: [
-        HttpModule
-      ]
-    });
+    TestBed.configureTestingModule(HttpMockResponse.testModuleMetaDataWith({
+      providers: [ReasonService]
+    }));
+
     mockBackend = getTestBed().get(MockBackend);
   }));
   
@@ -43,27 +22,10 @@ describe('Reason Service', () => {
     let reasonService: ReasonService;
 
     getTestBed().compileComponents().then(() => {
-      mockBackend.connections.subscribe(
-        (connection: MockConnection) => {
-          if (connection.request.url.endsWith('assets/Configs/Configs.json')){
-            console.log('Mocking config');
-            connection.mockRespond(new Response(
-              new ResponseOptions({
-                    body: [{
-                      wsUrl : "http://svc",
-                      GetStatustPollMilliSeconds : 1000,
-                      GetLogPollMilliSeconds : 1000,
-                      HomeURL : "http://home"
-                    }]
-                  })));
-          } else {
-            console.log('mocking status')
-            connection.mockRespond(new Response(
-              new ResponseOptions({
-                    body: {status: "submitted"}
-                  })));
-          }
-        });
+          HttpMockResponse.mockHttpCalls(mockBackend, [
+          HttpMockResponse.config(),
+          HttpMockResponse.withBody({status:"submitted"})
+        ]);
 
         reasonService = getTestBed().get(ReasonService);
         expect(reasonService).toBeDefined();
